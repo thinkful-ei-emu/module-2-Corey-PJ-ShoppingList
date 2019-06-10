f390cb9f0e7dc3daf9945cc6c70481ad3748bb62
Corey Moore <monorthwest@gmail.com>
2019-06-10 08:28:17 -0700

    added store.js, Item.js with testing errors in place

---
+++ scripts/store.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-undef */
+
+'use strict';
+
+const store = (function () {
+  const foo = 'bar';
+    
+  const store = {
+    items: [
+      { id: cuid(), name: 'oranges', checked: false },
+      { id: cuid(), name: 'milk', checked: true },
+      { id: cuid(), name: 'apples', checked: false },
+      { id: cuid(), name: 'bread', checked: false }
+    ],
+    hideCheckedItems: false,
+    searchTerm: ''
+  };
+
+  return {
+    items,
+    hideCheckedItems,
+    searchTerm
+  };
+}());
+console.log(store);
+console.log(foo);
